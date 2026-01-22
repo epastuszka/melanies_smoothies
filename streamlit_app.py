4c9f86117f5582a9b47fd43fcf4a4cfa657d1dc0
epastuszka <36705389+epastuszka@users.noreply.github.com>
2026-01-22 13:41:04 +0100

    Update streamlit_app.py
    removing tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,7 @@ if ingridient_list:
     for fruit_chosen in ingridient_list:
         ingredients_string +=fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
